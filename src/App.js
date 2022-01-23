9d5f1cbef9ad61e92bb24d30380399a49e3160cc
Gryphonne <hippogryphy@hotmail.com>
2022-01-23 20:29:16 +0100

    Added dependencies in app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ScoreSecondary from './components/score-secondary/ScoreSecondary';
+import SecondaryDropdown from './components/secondary-dropdown/SecondaryDropdown';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <ScoreSecondary/>
+        <SecondaryDropdown/>
       </header>
     </div>
   );
