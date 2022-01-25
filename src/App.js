788325018644476e6a2cf89c180dd4db569cc957
Gryphonne <hippogryphy@hotmail.com>
2022-01-25 21:18:21 +0100

    scaffold totalscore added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import ScoreCounter from './components/ScoreCounter';
 import SecondaryDropdown from "./components/SecondaryDropdown";
 import { Card } from '@mui/material';
 import PrimaryScoreCounter from './components/PrimaryScoreCounter';
+import { SecondaryScoreContext } from './context/SecondaryScoreContext';
 
 function App() {
+  const [secondaryScore, setSecondaryScore] = useState(0);
+  const [primaryScore, setPrimaryScore] = useState(0);
 
   return (
     <div className="App">
       <div className="player-container">
-        <div className="score-container">
-        <h3>Battle Round 2</h3><PrimaryScoreCounter/>
-        <h3>Battle Round 3</h3><PrimaryScoreCounter/>
-        <h3>Battle Round 4</h3><PrimaryScoreCounter/>
-        <h3>Battle Round 5</h3><PrimaryScoreCounter/>
-          <SecondaryDropdown/>
-          <ScoreCounter/>
-          <SecondaryDropdown/>
-          <ScoreCounter/>
-          <SecondaryDropdown/>
-          <ScoreCounter/>
-        </div>
+        <SecondaryScoreContext.Provider value={secondaryScore, setSecondaryScore}>
+          <div className="score-container">
+            <h3>Battle Round 2</h3><PrimaryScoreCounter/>
+            <h3>Battle Round 3</h3><PrimaryScoreCounter/>
+            <h3>Battle Round 4</h3><PrimaryScoreCounter/>
+            <h3>Battle Round 5</h3><PrimaryScoreCounter/>
+              <SecondaryDropdown/>
+              <ScoreCounter/>
+              <SecondaryDropdown/>
+              <ScoreCounter/>
+              <SecondaryDropdown/>
+              <ScoreCounter/>
+          </div>
+        </SecondaryScoreContext.Provider>
+
         <div className="score-container">
         <h3>Battle Round 2</h3><PrimaryScoreCounter/>
         <h3>Battle Round 3</h3><PrimaryScoreCounter/>
--- src/components/TotalScore.jsx
+++ src/components/TotalScore.jsx
@@ -1,4 +1,20 @@
 import { Box, Button, Container, Paper } from '@mui/material';
 import { Add, Clear, Remove } from '@mui/icons-material';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { SecondaryScoreContext } from '../context/SecondaryScoreContext';
 import './totalScore.css';
+
+function TotalScore() {
+
+    const {setSecondaryScore} = useContext(SecondaryScoreContext);
+
+    return (
+        <Container>
+            <Box className="flex-container">
+                <Paper className="ver hor big-font">{score}</Paper>
+            </Box>
+        </Container>
+    );
+}
+
+export default TotalScore;
